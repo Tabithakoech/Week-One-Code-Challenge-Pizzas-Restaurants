99cbf3309e354484c769eb5116d85bc1e3286ef1
TabithaKoech <tabbyk1718@gmail.com>
2022-10-09 19:40:03 +0300

    created a serializers folder

---
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+    def create
+       restaurant=RestaurantPizza.create!(restaurant_pizza_params)
+
+       render json: restaurant.pizza
+    end
+
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price,:pizza_id,:restaurant_id)
+    end
+
+    def unprocessable_entity_response(invalid)
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
+end

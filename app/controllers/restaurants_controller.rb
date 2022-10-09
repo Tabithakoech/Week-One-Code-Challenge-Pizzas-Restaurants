1d2e2851d56964acc273ee50e54fe8a649f92cc2
TabithaKoech <tabbyk1718@gmail.com>
2022-10-09 17:45:41 +0300

    Added a show ,delete route to restaurant controlle

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,24 @@ class RestaurantsController < ApplicationController
         restaurants = Restaurant.all
         render json: restaurants, status: :ok
       end
+      def show
+        restaurant = Restaurant.find_by!(id: params[:id])
+        render json: restaurant, serializer: RestaurantPizzaSerializer, status: :ok
+      end
+      def destroy
+        restaurant = find_restaurant
+        restaurant.destroy
+        head :no_content
+      end
+    
+      private
+    
+      def find_restaurant
+        Restaurant.find(params[:id])
+      end
+    
+      def render_not_found_response
+        render json: { error: "Restaurant not found"}, status: :not_found
+      end
+    
 end
